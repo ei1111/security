72074c87c6f4c285af44a6f35732a0004f33555a
Manjae kim <ei1111@naver.com>
2024-02-20 06:50:15 +0900

    security 테스트 Sha512 추가

---
+++ src/main/java/com/demo/common/config/Sha512PasswordEncoder.java
@@ -0,0 +1,40 @@
+
+package com.demo.common.config;
+
+import org.springframework.security.crypto.codec.Utf8;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Sha512PasswordEncoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return hashedSha512(rawPassword.toString());
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        String hashedPassword = encode(rawPassword);
+        return encodedPassword.equals(hashedPassword);
+    }
+
+    private String hashedSha512(String input) {
+        StringBuilder result = new StringBuilder();
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] digested = md.digest(input.getBytes());
+            for (int i=0; i<digested.length; i++) {
+                result.append(Integer.toHexString(0xFF & digested[i]));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return result.toString();
+    }
+}
+
